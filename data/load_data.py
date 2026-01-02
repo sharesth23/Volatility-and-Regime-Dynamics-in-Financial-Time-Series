e173aff88f9e942c8342ea30332df0d84b6635aa
Sharesth <sharesth444yt@gmail.com>
2026-01-02 16:09:29 +0530

    added part of code

--- data/load_data.py
+++ data/load_data.py
@@ -1,2 +1,13 @@
 import yfinance as yf
 import pandas as pd
+
+def load_sp500(start="2000-01-01"):
+    """
+    Loads S&P 500 daily prices and returns.
+    """
+    df = yf.download("^GSPC", start=start, progress=False)
+
+    prices = df["Adj Close"].dropna()
+    returns = prices.pct_change().dropna()
+
+    return prices, returns
